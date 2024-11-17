7b6ea059d364b2f278b6897e9becc4104a2f0237
ugrcnakis <ugrcnakis@gmail.com>
2024-11-17 23:48:56 +0300

    eda.py dosyamı src klasörüne yükledim.

---
+++ src/eda.py
@@ -0,0 +1,187 @@
+
+# Exploratory Data Analysis for Customer Segmentation Project
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+# !pip install missingno
+import missingno as msno
+from datetime import date
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+
+####################################################################################
+# * 1.2.Read the dataset*
+####################################################################################
+df = pd.read_csv("data/Mall_Customers.csv")
+
+
+# * Checking the data*
+def check_data(dataframe,head=5):
+    print(20*"-" + "Information".center(20) + 20*"-")
+    print(dataframe.info())
+    print(20*"-" + "Data Shape".center(20) + 20*"-")
+    print(dataframe.shape)
+    print("\n" + 20*"-" + "The First 5 Data".center(20) + 20*"-")
+    print(dataframe.head())
+    print("\n" + 20 * "-" + "The Last 5 Data".center(20) + 20 * "-")
+    print(dataframe.tail())
+    print("\n" + 20 * "-" + "Missing Values".center(20) + 20 * "-")
+    print(dataframe.isnull().sum())
+    print("\n" + 40 * "-" + "Describe the Data".center(40) + 40 * "-")
+    print(dataframe.describe([0.01, 0.05, 0.10, 0.50, 0.75, 0.90, 0.95, 0.99]).T)
+
+check_data(df)
+
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+
+##################################
+# KATEGORİK DEĞİŞKENLERİN ANALİZİ
+##################################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+
+
+
+##################################
+# NUMERİK DEĞİŞKENLERİN ANALİZİ
+##################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+df.head()
+
+
+# Gender Differences in Spending Scores and Incomes
+plt.figure(figsize=(14, 6))
+
+# Spending Score by Gender
+plt.subplot(1, 2, 1)
+sns.boxplot(x='Gender', y='Spending Score (1-100)', data=df)
+plt.title('Spending Score by Gender')
+
+# Annual Income by Gender
+plt.subplot(1, 2, 2)
+sns.boxplot(x='Gender', y='Annual Income (k$)', data=df)
+plt.title('Annual Income by Gender')
+
+plt.tight_layout()
+plt.show(block=True)
+
+
+# Relationship between Annual Income and Spending Score
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x='Annual Income (k$)', y='Spending Score (1-100)', data=df, hue='Gender', alpha=0.6)
+plt.title('Annual Income vs. Spending Score')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.show(block=True)
+
+
+# Relationship between Age and Spending Score
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x='Age', y='Spending Score (1-100)', data=df, hue='Gender', alpha=0.6)
+plt.title('Age vs. Spending Score')
+plt.xlabel('Age')
+plt.ylabel('Spending Score (1-100)')
+plt.show(block=True)
+
+
